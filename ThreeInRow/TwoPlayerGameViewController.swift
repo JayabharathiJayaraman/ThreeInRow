f76d4146bfeaec3ae9058c9a6b683ab5bf8d26e0
JayabharathiJayaraman <jayabharathi.jayaraman@iths.se>
2021-01-07 18:39:41 +0100

    Added scoring functionality for two player

--- ThreeInRow/TwoPlayerGameViewController.swift
+++ ThreeInRow/TwoPlayerGameViewController.swift
@@ -9,10 +9,23 @@ import UIKit
 
 class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
     
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    @IBOutlet weak var button4: UIButton!
+    @IBOutlet weak var button5: UIButton!
+    @IBOutlet weak var button6: UIButton!
+    @IBOutlet weak var button7: UIButton!
+    @IBOutlet weak var button8: UIButton!
+    @IBOutlet weak var button9: UIButton!
     @IBOutlet var crossButtons: [UIButton]!
+    
+    @IBOutlet weak var player1Score: UILabel!
+    @IBOutlet weak var player2Score: UILabel!
+    @IBOutlet weak var scoreLabel1: UILabel!
+    @IBOutlet weak var scoreLabel2: UILabel!
     @IBOutlet weak var playAgainButton: UIButton!
     @IBOutlet weak var winingPlayerLabel: UILabel!
-    
     @IBOutlet weak var gameDetailsTableView: UITableView!
     let personCellId = "personCellId"
     var player1Name : String?
@@ -21,23 +34,16 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
     var gameState = [0,0,0,0,0,0,0,0,0]
     let winingCombinations = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]]
     var gameIsActive = true
+    var score1: Int = 0
+    var score2: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let layer = CAGradientLayer()
-        layer.frame = view.bounds
-        layer.colors = [UIColor.blue]
-        layer.startPoint = CGPoint(x: 0,y: 0)
-        layer.endPoint = CGPoint(x: 1,y: 1)
-        view.layer.addSublayer(layer)
-        
-        
         gameDetailsTableView.dataSource = self
                 
-                let personCell = UINib(nibName: "GameDetailsTableViewCell", bundle: nil)
+        let personCell = UINib(nibName: "GameDetailsTableViewCell", bundle: nil)
                 
-                gameDetailsTableView.register(personCell, forCellReuseIdentifier: personCellId)
+        gameDetailsTableView.register(personCell, forCellReuseIdentifier: personCellId)
                 
         // Do any additional setup after loading the view.
         winingPlayerLabel.font = UIFont(name: "ChalkboardSE-Bold", size: winingPlayerLabel.font.pointSize)
@@ -46,6 +52,8 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
         if let player1Name = player1Name{
         showPlayerTurnToast(message: "\(player1Name) 's turn")
         }
+        scoreLabel1.text = player1Name
+        scoreLabel2.text = player2Name
     }
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
@@ -61,6 +69,7 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
     }
     
     @IBAction func playAgainButton(_ sender: Any) {
+        buttonsClickable()
         if let player2Name = player2Name{
         showPlayerTurnToast(message: "\(player2Name) 's turn")
         }
@@ -112,6 +121,8 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
                 if let player1Name = player1Name{
                 var winningPlayer1Name = "\(player1Name) has Won!"
                 winingPlayerLabel.text = winningPlayer1Name
+                    score1 += 1
+                    player1Score.text = String(score1)
                 }
                 
                 
@@ -119,11 +130,14 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
                 if let player2Name = player2Name{
                 var winningPlayer2Name = "\(player2Name) has Won!"
                 winingPlayerLabel.text = winningPlayer2Name
+                    score2 += 1
+                    player2Score.text = String(score2)
                 }
                 
             }
             playAgainButton.isHidden = false
             winingPlayerLabel.isHidden = false
+            buttonsUnclickable()
             
         }
         
@@ -144,9 +158,10 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
             winingPlayerLabel.text = "It was a draw!!"
             winingPlayerLabel.isHidden = false
             playAgainButton.isHidden = false
+            buttonsUnclickable()
             
         }
-        
+       
     }
     /*
     // MARK: - Navigation
@@ -178,4 +193,27 @@ class TwoPlayerGameViewController: UIViewController, UITableViewDataSource {
         
     }
  }
+    func buttonsUnclickable(){
+        button1.isUserInteractionEnabled = false
+        button2.isUserInteractionEnabled = false
+        button3.isUserInteractionEnabled = false
+        button4.isUserInteractionEnabled = false
+        button5.isUserInteractionEnabled = false
+        button6.isUserInteractionEnabled = false
+        button7.isUserInteractionEnabled = false
+        button8.isUserInteractionEnabled = false
+        button9.isUserInteractionEnabled = false
+    }
+    func buttonsClickable(){
+        button1.isUserInteractionEnabled = true
+        button2.isUserInteractionEnabled = true
+        button3.isUserInteractionEnabled = true
+        button4.isUserInteractionEnabled = true
+        button5.isUserInteractionEnabled = true
+        button6.isUserInteractionEnabled = true
+        button7.isUserInteractionEnabled = true
+        button8.isUserInteractionEnabled = true
+        button9.isUserInteractionEnabled = true
+    }
+    
 }
